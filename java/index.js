e2d211e642d0f16935b1cedae17d72e3a603c48f
sebastianpc88coder.io <perezcaro.seba@gmail.com>
2024-07-29 18:55:22 -0400

    Add files via upload

--- java/index.js
+++ java/index.js
@@ -3,23 +3,23 @@ reservaHora = "Reserva de Horas";
 console.log (reservaHora);
 //Valores de servicios
 const ESPACIO =" "
-const CORTECABELLO =12.000;
-const CORTEBARBA =6.000;
-const CABELLOYBARBA =18.000;
-const LIMPIEZAFACIAL =6.000;
-const SERVICIOFULL =23.000;
+const CORTECABELLO = 12.000;
+const CORTEBARBA = 6.000;
+const CABELLOYBARBA = 18.000;
+const LIMPIEZAFACIAL = 6.000;
+const SERVICIOFULL= 23.000;
 
 
 let nombreCompleto = prompt("Ingrese su Nombre");
 console.log("Nombre de Cliente:" +ESPACIO+ nombreCompleto)
 
-let servicio = prompt("Que servicio desea?  Corte de cabello | Corte de Barba | Cabello y Barba | Limpieza Facial | Servicio Full");
+let servicio = prompt("Que servicio desea?   Corte de cabello | Corte de Barba | Cabello y Barba | Limpieza Facial | Servicio Full");
 console.log("Servicio:" +ESPACIO+ servicio)
 
-//let fecha = prompt("Ingrese Dia - Mes - Año");
-//console.log("Fecha:" +ESPACIO+ fecha)
-let fecha = prompt(new Date(year,month,day)) 
-console.log("Fecha: " +ESPACIO+ fecha)
+//let valor =("valor a pagar: " +ESPACIO+ servicio == s1)
+
+let fecha = prompt("Ingrese Dia - Mes - Año");
+console.log("Fecha:" +ESPACIO+ fecha)
 
 let hora = prompt("Ingrese horario: 10:00  |  12:00  | 14:00  |  16:00  |  18:00  |  20:00");
 console.log("Horario:" +ESPACIO+ hora)
